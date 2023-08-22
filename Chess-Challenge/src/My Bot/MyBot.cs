d858cb932b413914eb44e1f0c257bf4bc41b5732
muhta <muhtasim.nabi@gmail.com>
2023-08-22 18:13:01 -0500

    bot going crazy

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,21 @@
 ﻿using ChessChallenge.API;
+using ChessChallenge.Example;
 
-public class MyBot : IChessBot
-{
-    public Move Think(Board board, Timer timer)
-    {
+public class MyBot : IChessBot {
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
+    public Move Think(Board board, Timer timer) {
         Move[] moves = board.GetLegalMoves();
+
+        foreach(Move move in moves) {
+            board.MakeMove(move);
+            bool checkmate = board.IsInCheckmate();
+            board.UndoMove(move);
+            if(checkmate) return move;
+
+            PieceType piece = move.CapturePieceType;
+            if (piece != 0) return move;
+        }
         return moves[0];
     }
 }
