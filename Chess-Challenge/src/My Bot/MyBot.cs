dd532a51c7b5e1b92c7dae6e10211232165f4f3b
muhta <muhtasim.nabi@gmail.com>
2023-08-22 18:46:46 -0500

    added evaluation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,11 +1,14 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 using ChessChallenge.Example;
 
 public class MyBot : IChessBot {
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    int[] pieceValues = { 0, 1, 3, 3, 5, 9, 100 };
 
     public Move Think(Board board, Timer timer) {
         Move[] moves = board.GetLegalMoves();
+
+        Console.WriteLine(Evaluation(board));
 
         foreach(Move move in moves) {
             board.MakeMove(move);
@@ -17,5 +20,20 @@ public class MyBot : IChessBot {
             if (piece != 0) return move;
         }
         return moves[0];
+    }
+
+    float Evaluation(Board board) {
+        float evaluation = 0;
+        for (int i = 0; i < 64; i++) {
+            int pieceType = (int) GetPiece(board, i).PieceType;
+            int value = pieceValues[pieceType];
+            if (GetPiece(board, i).IsWhite) evaluation += value;
+            else evaluation -= value;
+        }
+        return evaluation;
+    }
+
+    Piece GetPiece(Board board, int index) {
+        return board.GetPiece(new Square(index));
     }
 }
