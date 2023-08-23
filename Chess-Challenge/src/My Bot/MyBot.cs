2d91623452fbf1fdbd14a940cab1239dfe6549a3
muhta <muhtasim.nabi@gmail.com>
2023-08-23 18:37:32 -0500

    comments

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,21 +2,23 @@
 using ChessChallenge.API;
 
 public class MyBot : IChessBot {
-    int[] pieceValues = { 0, 1, 3, 3, 5, 9, 100 };
+    int[] pieceValues = { 0, 1, 3, 3, 5, 9, 20 };
 
     public Move Think(Board board, Timer timer) {
         Move[] moves = board.GetLegalMoves();
         Move move = moves[new Random().Next(moves.Length)];
-        float boardEval = Evaluation(board);
+        float boardEval = Evaluation(board); // get board evaluation before the bot makes a move
 
         foreach (Move possibleMove in moves) {
+            // check if checkmate, if so: make move GGs
             board.MakeMove(possibleMove);
             bool checkmate = board.IsInCheckmate();
             board.UndoMove(possibleMove);
             if (checkmate) return possibleMove;
 
+            // make a move, if that move improves eval: make move
             board.MakeMove(possibleMove);
-            float eval = -Evaluation(board);
+            float eval = -Evaluation(board); // get board evaluation after bot makes the possible move (it is techinically opponents turn right now, thats why we undo the move later)
             Console.WriteLine(eval + " ? " + boardEval);
             if (eval > boardEval) {
                 boardEval = eval;
@@ -38,7 +40,7 @@ public class MyBot : IChessBot {
             if (GetPiece(board, i).IsWhite) evaluation += value;
             else evaluation -= value;
         }
-        return board.IsWhiteToMove ? evaluation : -evaluation;
+        return board.IsWhiteToMove ? evaluation : -evaluation; // return board evaluation
     }
 
     Piece GetPiece(Board board, int index) {
