a6cc360cc832474dc250e901d7ea8a58f016d9f2
muhta <muhtasim.nabi@gmail.com>
2023-08-25 17:03:39 -0500

    added pawn points

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,23 +2,24 @@
 using ChessChallenge.API;
 
 public class MyBot : IChessBot {
+    public static string pawnPoints = "0000000043300334202112023302203340433404065445605786687578999987";
     int[] pieceValues = { 0, 1, 3, 3, 5, 9, 20 };
 
     public Move Think(Board board, Timer timer) {
         Move[] moves = board.GetLegalMoves();
         Move move = moves[new Random().Next(moves.Length)];
-        float boardEval = Evaluation(board); // get board evaluation before the bot makes a move
+        float boardEval = Evaluation(board, board.IsWhiteToMove); // get board evaluation before the bot makes a move
 
         foreach (Move possibleMove in moves) {
-            // check if checkmate, if so: make move GGs
+            // check if checkmate, if so: make move
             board.MakeMove(possibleMove);
             bool checkmate = board.IsInCheckmate();
             board.UndoMove(possibleMove);
             if (checkmate) return possibleMove;
 
-            // make a move, if that move improves eval: make move
+            // make a move, if that move improves eval: confirm move
             board.MakeMove(possibleMove);
-            float eval = -Evaluation(board); // get board evaluation after bot makes the possible move (it is techinically opponents turn right now, thats why we undo the move later)
+            float eval = Evaluation(board, !board.IsWhiteToMove); // get board evaluation after bot makes the possible move (it is techinically opponents turn right now, thats why we undo the move later)
             Console.WriteLine(eval + " ? " + boardEval);
             if (eval > boardEval) {
                 boardEval = eval;
@@ -28,19 +29,28 @@ public class MyBot : IChessBot {
         }
 
         Console.WriteLine(move);
+        Console.WriteLine(Evaluation(board, board.IsWhiteToMove));
+        board.MakeMove(move);
+        Console.WriteLine(Evaluation(board, board.IsWhiteToMove));
+        board.UndoMove(move);
 
         return move;
     }
 
-    float Evaluation(Board board) {
+    float Evaluation(Board board, bool white) {
         float evaluation = 0;
         for (int i = 0; i < 64; i++) {
             int pieceType = (int) GetPiece(board, i).PieceType;
             int value = pieceValues[pieceType];
+            if (white && pieceType == 1) {
+                value *= byte.Parse(pawnPoints[i] + "");
+            } else if (pieceType == 1) {
+                value *= byte.Parse(pawnPoints[63 - i] + "");
+            }
             if (GetPiece(board, i).IsWhite) evaluation += value;
             else evaluation -= value;
         }
-        return board.IsWhiteToMove ? evaluation : -evaluation; // return board evaluation
+        return white ? evaluation : -evaluation; // return board evaluation
     }
 
     Piece GetPiece(Board board, int index) {
